d118d6a61b765d5c51d40c3dc2f7180f96f935d9
AronGida <gidaaron@gmail.com>
2019-04-16 14:30:21 +0200

    refactored project structure

---
+++ src/test/java/com/codecool/TestLogin.java
@@ -0,0 +1,40 @@
+package com.codecool;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+
+public class TestLogin {
+    private Login login;
+
+    @BeforeEach
+    void setLogin() {
+        login = new Login(new Driver().getDriver());
+    }
+
+    @AfterEach
+    void closeDriver(){
+        login.close();
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/valid_login.csv", numLinesToSkip = 1)
+     void loginSuccesful(String username, String password, String expectedresult){
+        login.openLoginPage();
+        login.fullLogin(username, password);
+        assertEquals(expectedresult, login.getUsername());
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/invalid_login.csv", numLinesToSkip = 1)
+     void loginFailure(String username, String password){
+        login.openLoginPage();
+        login.fullLogin(username,password);
+        assertEquals(login.getURL(), login.LOGIN_URL);
+    }
+}
